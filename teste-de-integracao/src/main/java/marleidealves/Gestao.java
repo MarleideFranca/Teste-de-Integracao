6bdfa4436527a152b3bf7cf9d2f377b89fced0ab
@marleidealves <marleidealves2002@yahoo.com.br>
2025-07-03 19:43:49 -0300

    Correção da classe Gestao

--- teste-de-integracao/src/main/java/marleidealves/Gestao.java
+++ teste-de-integracao/src/main/java/marleidealves/Gestao.java
@@ -1,5 +1,20 @@
 package marleidealves;
 
 public class Gestao {
-    
+    private Pedido pedido;
+
+    // Construtor
+    public Gestao(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    // Método para calcular o valor total da venda
+    public double valorTotalVenda() {
+        double total = 0.0;
+        for (Produto produto : pedido.getProdutos()) {
+            total += produto.getPreco();
+        }
+        return total;
+    }
 }
+
