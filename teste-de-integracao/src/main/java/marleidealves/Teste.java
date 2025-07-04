2d7313927dd57bb5b12f978f759b89226329cb36
@marleidealves <marleidealves2002@yahoo.com.br>
2025-07-04 12:50:05 -0300

    Validação da classe Teste

--- teste-de-integracao/src/main/java/marleidealves/Teste.java
+++ teste-de-integracao/src/main/java/marleidealves/Teste.java
@@ -1,7 +1,10 @@
 package marleidealves;
 
-public class Teste { 
-    public double calculo(double a, double b, double c){ 
-        return (a + b) / c; 
-    } 
+public class Teste {
+    public double calculo(double a, double b, double c) {
+        if (c == 0) {
+            throw new IllegalArgumentException("Divisão por zero não permitida.");
+        }
+        return (a + b) / c;
+    }
 }
